bec2aab0bfdbb74f412a1718a8680f990d847bbe
khadmalek <k.abdelmalek@live.fr>
2024-10-29 09:44:31 +0100

    modification du code - méthode ajustée + ajout de parametre

--- Poisson.py
+++ Poisson.py
@@ -1,5 +1,5 @@
 import random
-from Requin import Requin
+# from Requin import Requin
 
 class Poisson:
     
@@ -7,76 +7,71 @@ class Poisson:
 
         self.emplacement_x = x  # Position x du poisson dans la grille
         self.emplacement_y = y  # Position y du poisson dans la grille
-        self.temps_reproduction_poisson = temps_reproduction_poisson  # Temps requis pour la reproduction
-        self.temps_reproduction = 0  # Compteur de reproduction initialisé à 0
-        self.ancien_emplacement = [self.emplacement_x, self.emplacement_y]
+        self.temps_reproduction_poisson = temps_reproduction_poisson 
+        self.temps_reproduction = 0  
+        self.ancien_emplacement = [self.emplacement_x, self.emplacement_y]  # Dernière position avant déplacement
 
-
-    def cases_voisines(self) -> list[tuple]:
-
+    def cases_voisines(self, longueur):
+       
         return [
-            (mouvement_thoroidal(self.emplacement_x+1), self.emplacement_y), 
-            (mouvement_thoroidal(self.emplacement_x-1), self.emplacement_y),
-            (self.emplacement_x, mouvement_thoroidal(self.emplacement_y+1)), 
-            (self.emplacement_x, mouvement_thoroidal(self.emplacement_y-1))
+            (mouvement_thoroidal(self.emplacement_x + 1, longueur), self.emplacement_y), 
+            (mouvement_thoroidal(self.emplacement_x - 1, longueur), self.emplacement_y),
+            (self.emplacement_x, mouvement_thoroidal(self.emplacement_y + 1, longueur)), 
+            (self.emplacement_x, mouvement_thoroidal(self.emplacement_y - 1, longueur))
         ]
 
-    def deplacement(self):
-
-        # stocker l'ancien emplacement du poisson
+    def deplacement(self, longueur):
+        
+        # Stocke la position actuelle comme ancien emplacement
         self.ancien_emplacement = [self.emplacement_x, self.emplacement_y]
-        # Obtenir les cases libres autour du poisson
-        cases_voisines = self.cases_voisines()
 
-        for _ in range(len(cases_voisines)) :
-            # Choisit une case libre au hasard parmi les voisins libres
+        cases_voisines = self.cases_voisines(longueur)
+
+        for _ in range(len(cases_voisines)):
             case_choisie = random.choice(cases_voisines)
+
+            # Vérifie si la case est libre (ni requin ni poisson)
+            x, y = case_choisie
             if not isinstance(case_choisie, Requin) and not isinstance(case_choisie, Poisson):
+                # Déplace le poisson à la nouvelle position
                 self.emplacement_x, self.emplacement_y = case_choisie
                 break
-            else : 
+            else:
+                # Retire la case de la liste si elle n'est pas libre
                 cases_voisines.remove(case_choisie)
+
         # Incrémente le compteur de reproduction après le déplacement
         self.temps_reproduction += 1
+
+    def reproduction(self, liste_animaux):
         
-
-    def reproduction(self, liste_animaux : list["Poisson", "Requin"]):
-
-        # Vérifie si le compteur de reproduction a atteint le cycle requis
-        if self.temps_reproduction >= self.temps_reproduction_poisson and [self.emplacement_x, self.emplacement_y] != self.ancien_emplacement:
+        # Vérifie si le cycle de reproduction est atteint et si l'ancien emplacement est différent
+        if self.temps_reproduction >= self.temps_reproduction_poisson and \
+           [self.emplacement_x, self.emplacement_y] != self.ancien_emplacement:
+            
+            # Coordonnées de l'ancien emplacement pour le nouveau poisson
             nouveau_x, nouveau_y = self.ancien_emplacement
             
-            # Crée un nouveau poisson et le place dans la grille
+            # Crée un nouveau poisson avec les mêmes paramètres de reproduction
             nouveau_poisson = Poisson(nouveau_x, nouveau_y, self.temps_reproduction_poisson)
             
-            # on ajoute le nouveau poisson à la liste d'animaux
             liste_animaux.append(nouveau_poisson)
             
-            # Réinitialise le compteur de reproduction
             self.temps_reproduction = 0
-                
-    
-    def __str__(self):
 
+    def __str__(self):
+       
         return " 🐟"
 
 
-# Autres fonctions
-
-def mouvement_thoroidal(coord, longueur) : 
-    if coord == longueur : 
+# autes Fonction
+def mouvement_thoroidal(coord, longueur):
+   
+    if coord == longueur:  
         return 0
-    elif coord == -1 : 
-        return longueur-1
-    # return coord % longueur
-
-
-def mouvement_thoroidal(coord, longueur) : 
-    if coord == longueur : 
-        return 0
-    elif coord == -1 : 
-        return longueur-1
-    # return coord % longueur
+    elif coord == -1:  
+        return longueur - 1
+    return coord 
 
 
 
