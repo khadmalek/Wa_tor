8a6f56047716b8a4d51daf87a3eddb924954ec4d
Malek BOUMEDINE <malek.boumedine@gmail.com>
2024-10-30 14:46:32 +0100

    finalisation fichier planete

--- Planete.py
+++ Planete.py
@@ -1,45 +1,53 @@
-from Poisson import Poisson
-from Requin import Requin
+from Animaux import Poisson, Requin
 import itertools
 
+
+################################################################################################
+
 class Planete : 
+
     def __init__(self, largeur : int, hauteur : int):
         self.__largeur = largeur
-        self.__longueur = hauteur        
+        self.__hauteur = hauteur        
 
 
     def affichage_grille(self, liste_entites: list["Poisson", "Requin"]) -> None:
         
-        # Stocker les coordonnées et le __str__ des entités dans un dictionnaire
+        # stocker les coordonnées et le __str__ des entités dans un dictionnaire
         grille_entites = {(entite.emplacement_x, entite.emplacement_y): str(entite) for entite in liste_entites}
         
-        # Toutes les coordonnées de la planète (selon la longueur et la largeur)
-        coordonnees = list(itertools.product(range(self.__largeur), range(self.__longueur)))
+        # toutes les coordonnées de la planète (selon la hauteur et la largeur)
+        coordonnees = list(itertools.product(range(self.__largeur), range(self.__hauteur)))
         
-        # Création d'une grille vide
-        grille_vide = {coord: " o " for coord in coordonnees}
+        # création d'une grille vide
+        grille_vide = {coord : "    " for coord in coordonnees}
         
-        # Ajout de cases vides aux coordonnées non occupées de la grille des entités
-        for coord in grille_vide:
+        # ajout de cases vides aux coordonnées non occupées de la grille des entités
+        for coord in grille_vide.keys():
             if coord not in grille_entites:
                 grille_entites[coord] = grille_vide[coord]
         
-        # Ordonner la grille avant de l'afficher
-        grille_ordonnee = sorted(grille_entites.items(), key=lambda x: (x[0][0], x[0][1]))
+        # ordonner la grille avant de l'afficher
+        grille_ordonnee = dict(sorted(grille_entites.items(), key=lambda x: (x[0][0], x[0][1])))
         
-        # Splitter la grille en plusieurs sous-listes pour simplifier l'affichage
-        grille_a_afficher = [grille_ordonnee[i:i+self.__largeur] for i in range(0, len(grille_ordonnee), self.__largeur)]
-        
-        # Affichage de la grille
-        for ligne in grille_a_afficher:
-            print(" | ".join(value for _, value in ligne))
-  
-  
-            
+        # splitter la grille en plusieurs sous-listes pour simplifier l'affichage
+        grille_items = list(grille_ordonnee.values())
+        grille_a_afficher = [[item for item in grille_items[i:i+self.__largeur]] for i in range(0, len(grille_ordonnee), self.__largeur)]
         
         
+        # affichage de la grille
+        separateur = "-" + "-" * (self.__largeur * 4 + (self.__largeur - 1))
+        print(separateur)
+        for ligne in grille_a_afficher :
+            for e in ligne : 
+                print(e, end="|")
+            print()
+            print(separateur)
 
-# affichage du nombre de poissons et de requins
+            
+################################################################################################
+
+
 def afficher_chiffres(list_entites : list["Poisson", "Requin"]) : 
     
     nombre_poissons = len([e for e in list_entites if isinstance(e, Poisson) and not isinstance(e, Requin)])
@@ -47,18 +55,16 @@ def afficher_chiffres(list_entites : list["Poisson", "Requin"]) :
     print(f"Nombre de poissons : {nombre_poissons} \nNombre de requins : {nombre_requins} ")
 
 
-
 def statistiques() : 
     pass
 
 
-
-
-def afficher_statistiques (list_entites : list["Poisson", "Requin"]) : 
-  
-    nombre_poissons = len([e for e in list_entites if isinstance(e, Poisson) and not isinstance(e, Requin)])
-    nombre_requins = len([e for e in list_entites if isinstance(e, Requin)])
-    print(f"Nombre de poissons : {nombre_poissons} \nNombre de requins : {nombre_requins} ")
+if __name__ == "__main__" : 
+    
+    l1 = [Poisson(0,0,5), Poisson(0,1,5), Poisson(1,0,5), Poisson(1,1,5)]
+    planete1 = Planete(5,5)
+    planete1.affichage_grille(l1)
+    afficher_chiffres(l1)
 
 
 
