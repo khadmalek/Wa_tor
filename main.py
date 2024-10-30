f693fe8a5f0198209ef894d00e1ea012c19e1e69
khadmalek <k.abdelmalek@live.fr>
2024-10-30 14:49:52 +0100

    remodelisation du fichier main et correction des bugs - ajout du configparser

--- main.py
+++ main.py
@@ -3,70 +3,72 @@ from Poisson import Poisson
 from Requin import Requin
 from Planete import Planete
 from Planete import afficher_chiffres
+import os
+import time
+import configparser
+
 
 # Paramètres de la simulation
-largeur = 15
-hauteur = 15
-nombre_poisson_initial = 80
-nombre_requin_initial = 10
-energie_initiale = 10
-temps_de_reproduction_poisson = 3
-temps_de_reproduction_requin = 8
-gain_energie_par_poisson = 6
+
+config = configparser.ConfigParser()
+config.read('parametre.ini')
+
+largeur = config['largeur']
+hauteur = config['hauteur']
+nombre_poisson_initial = config['nombre_poisson_initial']
+nombre_requin_initial = config['nombre_requin_initial']
+energie_initiale = config['energie_initiale']
+temps_de_reproduction_poisson = config['temps_de_reproduction_poisson']
+temps_de_reproduction_requin = ['temps_de_reproduction_requin']
+gain_energie_par_poisson = config['gain_energie_par_poisson']
 
 
 ####################################################################################
 
 
+# création d'une fonction qui créée la liste d'animaux
 
+def creer_animaux(largeur : int, hauteur : int) -> list :
+    
+    cases_vides = [(x, y) for x in range(largeur) for y in range(hauteur)]
+    liste_animaux = []
+    for _ in range(nombre_poisson_initial):
+        x, y = random.choice(cases_vides)
+        liste_animaux.append(Poisson(x, y, temps_de_reproduction_poisson))
+        cases_vides.remove((x, y))
 
-
-
-
-
-
+    for _ in range(nombre_requin_initial):
+        x, y = random.choice(cases_vides)
+        liste_animaux.append(Requin(x, y, temps_de_reproduction_requin, energie_initiale))
+        cases_vides.remove((x, y))
+    return liste_animaux
 
 
 ####################################################################################
 
 # Création de la planète
 aqualand = Planete(largeur, hauteur)
+liste_animaux = creer_animaux(largeur, hauteur)
 
-# Initialisation des listes d'animaux et des cases vides
-liste_animaux = []
-liste_poissons = []
-liste_requins = []
-cases_vides = [(x, y) for x in range(largeur) for y in range(hauteur)]
-
-# Création des requins
-for _ in range(nombre_requin_initial):
-    x, y = random.choice(cases_vides)
-    liste_animaux.append(Requin(x, y, temps_de_reproduction_requin, energie_initiale))
-    cases_vides.remove((x, y))
-
-# Création des poissons
-for _ in range(nombre_poisson_initial):
-    x, y = random.choice(cases_vides)
-    liste_animaux.append(Poisson(x, y, temps_de_reproduction_poisson))
-    cases_vides.remove((x, y))
 
 chronon = 0
 while True:
-    for animal in liste_animaux:
-        if isinstance(animal, Poisson) and not isinstance(animal, Requin):
-            animal.deplacement(largeur)
-            animal.reproduction(liste_animaux)
-    
-    for animal in liste_animaux:
-        if isinstance(animal, Requin):
-            animal.deplacement(liste_animaux)       # et manger poisson en meme temps
-            animal.reproduction_requin(liste_animaux)
-            animal.manger_poisson(liste_animaux)        # a supprimer
-            animal.mourir(liste_animaux)
-    
-    # Afficher la grille et les statistiques
-    print(f"\nChronon : {chronon}")
-    aqualand.affichage_grille(liste_animaux)
-    afficher_chiffres(liste_animaux)
+    if chronon % 10 == 0 :
+        for animal in liste_animaux :
+            if isinstance(animal, Poisson) and not isinstance(animal, Requin):
+                animal.deplacement(liste_animaux,largeur, hauteur)
+                animal.reproduction(liste_animaux)
+        
+        for animal in liste_animaux:
+            if isinstance(animal, Requin):
+                animal.deplacement(liste_animaux, largeur, hauteur)       # et manger poisson en meme temps
+                animal.reproduction_requin(liste_animaux)
+                animal.mourir(liste_animaux)
+        
+        # Afficher la grille et les statistiques
+        os.system("clear")
+        aqualand.affichage_grille(liste_animaux)
+        afficher_chiffres(liste_animaux)
+        time.sleep(0.3)
     
     chronon += 1
